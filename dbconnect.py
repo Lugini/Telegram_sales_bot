c0b763cc703609c1ed6389c4df2c0d9705f9702e
Lugini <artur.dartur@gmail.com>
2018-07-05 20:41:28 +0300

    Improved deamon

--- dbconnect.py
+++ dbconnect.py
@@ -26,3 +26,30 @@ class DBHelper:
     def get_items(self):
         stmt = "SELECT description FROM items"
         return [x[0] for x in self.conn.execute(stmt)]
+
+
+class Sent:
+    def __init__(self, dbname="todo.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    def setup(self):
+        stmt = "CREATE TABLE IF NOT EXISTS sent (description text)"
+        self.conn.execute(stmt)
+        self.conn.commit()
+
+    def add_item(self, item_text):
+        stmt = "INSERT INTO sent (description) VALUES (?)"
+        args = (item_text, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_item(self, item_text):
+        stmt = "DELETE FROM sent WHERE description = (?)"
+        args = (item_text, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def get_items(self):
+        stmt = "SELECT description FROM sent"
+        return [x[0] for x in self.conn.execute(stmt)]
--- reddit_deamon.py
+++ reddit_deamon.py
@@ -2,7 +2,7 @@ import sqlite3
 
 import praw
 import configs
-from dbconnect import DBHelper
+from dbconnect import DBHelper, Sent
 
 
 class URLAnalyzer(object):
@@ -44,26 +44,50 @@ def main_deamon(user_id = None):
 
     subreddit = reddit.subreddit('GameDeals')
     i=0
+    not_sent = 0
     with sqlite3.connect("todo.sqlite") as con:
+        sent_table = Sent()
+        sent_table.setup()
         db = DBHelper()
         for submission_id in subreddit.stream.submissions():
             try:
                 i += 1
-                print('Post number_{}_'.format(i))
                 submission = reddit.submission(id=submission_id)
                 if submission.ups >= configs.MIN_UPVOTES and url_analyzer.is_ref_link(submission.url):
+                    not_sent += 1
                     new_url = url_analyzer.ref_code_replace(submission.url)
+                    exists = sent_table.get_items()
+                    if new_url in exists:
+                        not_sent += 1
+                        print('Post_{}_existing_not_sent {}'.format(i, not_sent))
+                        continue
                     if user_id:
-                        configs.bot.sendMessage(user_id,"{}\n{}".format(submission.title, new_url))
+                        not_sent += 1
+                        configs.bot.sendMessage(user_id, "{}\n{}".format(submission.title, new_url))
                     else:
+                        print('Post_{}_ref_sent {}'.format(i, not_sent))
                         for user in db.get_items():
                             configs.bot.sendMessage(user, "{}\n{}".format(submission.title, new_url))
+                        sent_table.add_item(new_url)
+                elif i >= 100 and not_sent >= 15 and "reddit.com" not in submission.url:
+                    not_sent = 0
+                    sent_table.add_item(new_url)
+                    for user in db.get_items():
+                        configs.bot.sendMessage(user, "{}\n{}".format(submission.title, submission.url))
+                    print('Post_{}_sent'.format(i))
+                else:
+                    not_sent += 1
+                    print('Post_{}_not_sent {}'.format(i, not_sent))
                 if user_id and i == 100:
                     return
                 else:
                     continue
             except praw.exceptions.PRAWException as e:
                 pass
+            except Exception as e:
+                print("Not sent error")
+                continue
+
 
 
 if __name__ == '__main__':
